9d95f0d7e7881c38e0957fcedb618d38b91fae91
Bobai Kato <bobaikato@gmail.com>
2019-12-27 12:59:23 +0000

    Added Condition Test
    Signed-off-by: Bobai Kato <bobaikato@gmail.com>

---
+++ src/test/java/functions/ConditionTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2018 — 2019 Honerfor, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package functions;
+
+import com.honerfor.cutils.Que;
+import com.honerfor.cutils.function.Condition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Test Custom Functions")
+final class ConditionTest {
+
+    @DisplayName("Should return Boolean True based on the given conditions.")
+    @ParameterizedTest(name = "{index} => value={0}")
+    @MethodSource("trueConditionFunctions")
+    void verifyReturnExpectedValuesOfSameType(Condition condition) {
+        Que.run(() -> assertTrue(condition.isMet()))
+                .andRun(() -> assertFalse(condition.isNotMet()));
+    }
+
+    private static Stream<Arguments> trueConditionFunctions() {
+        final Condition firstCondition = () -> true;
+        final Condition secondCondition = () -> true;
+
+        return Stream.of(
+                Arguments.of(firstCondition),
+                Arguments.of(secondCondition)
+        );
+    }
+
+
+    @DisplayName("Should return Boolean False based on the given conditions.")
+    @ParameterizedTest(name = "{index} => value={0}")
+    @MethodSource("falseConditionFunctions")
+    void verifyDealerThrowsException(Condition condition) {
+        Que.run(() -> assertFalse(condition.isMet()))
+                .andRun(() -> assertTrue(condition.isNotMet()));
+    }
+
+    private static Stream<Arguments> falseConditionFunctions() {
+        final Condition firstCondition = () -> false;
+        final Condition secondCondition = () -> false;
+
+        return Stream.of(
+                Arguments.of(firstCondition),
+                Arguments.of(secondCondition)
+        );
+    }
+}
