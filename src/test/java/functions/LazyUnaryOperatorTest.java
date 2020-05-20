8e4356540e0f576ab470e4ab8a80bdc50ad7522c
Bobai Kato <bobaikato@gmail.com>
2020-05-20 12:52:25 +0100

    Added Lazy unary operator Test
    Signed-off-by: Bobai Kato <bobaikato@gmail.com>

---
+++ src/test/java/functions/LazyUnaryOperatorTest.java
@@ -0,0 +1,92 @@
+/*
+ * _________  ____ ______________.___.____       _________
+ * \_   ___ \|    |   \__    ___/|   |    |     /   _____/
+ * /    \  \/|    |   / |    |   |   |    |     \_____  \
+ * \     \___|    |  /  |    |   |   |    |___  /        \
+ *  \______  /______/   |____|   |___|_______ \/_______  /
+ *         \/                                \/        \/
+ *
+ * Copyright (C) 2018 — 2020 Honerfor, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package functions;
+
+import static java.lang.System.nanoTime;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import com.honerfor.cutils.function.LazyUnaryOperator;
+import com.honerfor.cutils.function.ThrowingUnaryOperation;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class LazyUnaryOperatorTest {
+
+  static final int MOCK_LATENCY = 2000;
+
+  @DisplayName("Expect Lazy Function to memoize values for Functions of same argument.")
+  @ParameterizedTest(name = "{index} =>  input={1}  second={2}")
+  @MethodSource("lazyFunctionOperations")
+  void verifyLazyUnaryOperatorValues(
+    final UnaryOperator<String> operator, final String input, int sec) {
+
+    final long startTime = nanoTime();
+
+    final String result = operator.apply(input);
+
+    final long endTime = nanoTime();
+    final long executionTime = SECONDS.convert((endTime - startTime), NANOSECONDS);
+
+    Assertions.assertEquals(MOCK_LATENCY + input, result); // result check
+    Assertions.assertEquals(sec, executionTime); // check execution time
+  }
+
+  private static Stream<Arguments> lazyFunctionOperations() {
+
+    final UnaryOperator<String> function =
+      LazyUnaryOperator.of(
+        ThrowingUnaryOperation.unchecked(
+          value -> {
+            final int time = MOCK_LATENCY;
+            try {
+              Thread.sleep(time); // mock operation with high latency
+            } catch (InterruptedException e) {
+            }
+            return time + value;
+          }));
+
+    return Stream.of(
+      Arguments.of(function, "23", 2),
+      Arguments.of(function, "23", 0),
+      Arguments.of(function, "24", 2),
+      Arguments.of(function, "23", 0),
+      Arguments.of(function, "23", 0),
+      Arguments.of(function, "25", 2),
+      Arguments.of(function, "24", 0),
+      Arguments.of(function, "23", 0),
+      Arguments.of(function, "25", 0),
+      Arguments.of(function, "23", 0),
+      Arguments.of(function, "26", 2),
+      Arguments.of(function, "24", 0),
+      Arguments.of(function, "23", 0),
+      Arguments.of(function, "26", 0),
+      Arguments.of(function, "24", 0));
+  }
+}
