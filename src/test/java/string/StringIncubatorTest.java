4c6138b6b0e2651639d189d6878f87346bbfdf16
Bobai Kato <bobaikato@gmail.com>
2021-07-10 15:51:59 +0100

    clean up test
    Signed-off-by: Bobai Kato <bobaikato@gmail.com>

--- src/test/java/string/StringIncubatorTest.java
+++ src/test/java/string/StringIncubatorTest.java
@@ -24,6 +24,7 @@ package string;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.honerfor.cutils.string.StringIncubator;
@@ -62,8 +63,8 @@ final class StringIncubatorTest {
   @ParameterizedTest(name = "{index} => First hatch={0}, Second hatch={1}")
   @MethodSource("resourceII")
   void shouldHatchUniqueStringsOfSpecifyLength(String firstString, String secondString) {
-    assertNotEquals(firstString, null);
-    assertNotEquals(secondString, null);
+    assertNotNull(firstString);
+    assertNotNull(secondString);
     assertNotEquals(firstString, secondString);
   }
 
@@ -80,13 +81,13 @@ final class StringIncubatorTest {
   @DisplayName("Should successfully hatch String with 64 characters.")
   void defaultHatchStringSizeShouldBe64() {
     final String hatchedString = new StringIncubator().hatch();
-    assertEquals(hatchedString.length(), 64);
+    assertEquals(64,hatchedString.length());
   }
 
   @Test
   @DisplayName(
       "Should successfully hatch String from supplied symbol of specified length characters.")
-  void defaultHatchStringSizeShouldBeOFSpecifiedSizeAndSymbol() {
+  void defaultHatchStringSizeShouldBeOfSpecifiedSizeAndSymbol() {
     final String hatchedString = new StringIncubator(12, "AaDdZz").hatch();
     assertEquals(hatchedString.length(), 12);
   }
